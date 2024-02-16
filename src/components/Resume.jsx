ace40ab53a663dc2cfbef5bf99eb931478565c56
Kaleab <kaleab83@gmail.com>
2024-02-15 19:19:46 -0500

    Resume fixed styling needed. Starting work on todolist

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -3,6 +3,8 @@ import Container from '@mui/material/Container';
 import NavBar from './NavBar';
 import Paper from '@mui/material/Paper';
 import PortfolioResume from '../documents/PortfolioResume.pdf'
+import "react-pdf/dist/esm/Page/TextLayer.css";
+import "react-pdf/dist/esm/Page/TextLayer.css"
 import {Document, Page, pdfjs} from 'react-pdf'
 pdfjs.GlobalWorkerOptions.workerSrc = 'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.11.174/pdf.worker.js'
 
@@ -14,13 +16,25 @@ export default function Resume() {
       <NavBar/>
     </Container>
 
-    <Container maxWidth="sm" style={{textAlign: 'center', marginTop: '100px'}}>
-      <Paper>
+    <Container maxWidth='sm' style={{marginTop: '100px', textDecoration: 'underline', maxWidth: 'fit-content',
+      marginLeft: 'auto',
+      marginRight: 'auto'}}>
+      <h1>Resume</h1>
+    </Container>
+
+    <Container maxWidth="sm" 
+    style={{
+      marginTop: '100px',  
+      maxWidth: 'fit-content',
+      marginLeft: 'auto',
+      marginRight: 'auto'
+      }}>
+      <Paper >
       <Document
       file={PortfolioResume}
       onLoadError={console.error}
-      style={{ width: '100vw', height: 'auto'}}>
-      <Page pageIndex={0}/>
+    >
+      <Page pageIndex={0}  scale={2.0} renderTextLayer={false} renderAnnotationLayer={false}/>
       </Document>
       </Paper>
     </Container>
